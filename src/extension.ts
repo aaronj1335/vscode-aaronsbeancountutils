800bb5e3f6c3be0c8f3983195a1f0797cb1ee820
Aaron Stacy <aaronstacy@google.com>
2021-09-05 23:39:55 -0500

    finish 'calc and copy' command

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,27 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { commands, ExtensionContext, window } from "vscode";
+import { commands, env, ExtensionContext, window } from "vscode";
+import calculator = require('./calculator');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	// console.log('Congratulations, your extension "aaronsbeancountutils" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = commands.registerCommand('aaronsbeancountutils.calc', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		// vscode.window.showInformationMessage('Hello World from AaronsBeancountUtils!');
+function calcAndCopy() {
 		const editor = window.activeTextEditor;
 		if (editor == null) return;
-		console.log(editor.selection.start.line);
-	});
 
-	context.subscriptions.push(disposable);
+		const currentLine = editor.document.lineAt(editor.selection.start.line);
+		const result = calculator.parse(currentLine.text.replace(/^;/, ''))
+			.toString();
+
+		editor.edit(editBuilder => {
+			editBuilder.replace(currentLine.range, `${currentLine.text} = ${result}`);
+		});
+
+		env.clipboard.writeText(result);
+}
+
+export function activate(context: ExtensionContext) {
+	context.subscriptions
+	  .push(commands.registerCommand('aaronsbeancountutils.calc', calcAndCopy));
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+// export function deactivate() { }
