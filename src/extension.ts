91ff74f0f7215e6ca9814a3d100692a727e5d801
Aaron Stacy <aaronstacy@google.com>
2021-09-07 22:16:36 -0500

    make calc cmd work on postings w/ cost

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,33 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import Decimal from "decimal.js";
 import { commands, env, ExtensionContext, window } from "vscode";
 import calculator = require('./calculator');
+
+const POSTING = /^\s+[\w:]+\s+([0-9.-]+)\s+\w+\s+\{([0-9.-]+)/
 
 function calcAndCopy() {
 		const editor = window.activeTextEditor;
 		if (editor == null) return;
 
 		const currentLine = editor.document.lineAt(editor.selection.start.line);
-		const result = calculator.parse(currentLine.text.replace(/^;/, ''))
-			.toString();
+		const [result, newLine] = calculate(currentLine.text);
 
-		editor.edit(editBuilder => {
-			editBuilder.replace(currentLine.range, `${currentLine.text} = ${result}`);
-		});
+		editor.edit(editBuilder => editBuilder.replace(currentLine.range, newLine));
+		env.clipboard.writeText(result.toString());
+}
 
-		env.clipboard.writeText(result);
+export function calculate(line: string): [Decimal, string] {
+	const match = POSTING.exec(line)
+	let result: Decimal;
+	if (match) {
+		const [_, amount, cost, ...rest] = match;
+		result = new Decimal(amount).times(cost);
+		line += ' ;';
+	} else {
+		result = calculator.parse(line.replace(/^\s*;/, ''));
+	}
+	return [result, `${line} = ${result.toString()}`];
 }
 
 export function activate(context: ExtensionContext) {
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,30 @@
 import * as assert from 'assert';
+import Decimal from 'decimal.js';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import {calculate} from '../../extension';
 
-suite('Extension Test Suite', () => {
+suite('extension', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('calculate', () => {
+		const [result, line] = calculate('0.1 + 0.2');
+		assert(result.equals(new Decimal('0.3')));
+		assert.strictEqual(line, '0.1 + 0.2 = 0.3');
+	});
+
+	test('calculate.withLeadingComment', () => {
+		const [result, line] = calculate('; 1 + 2');
+		assert(result.equals(new Decimal('3')));
+		assert.strictEqual(line, '; 1 + 2 = 3');
+	});
+
+	test('calculate.amountWithCost', () => {
+		const line = '  Assets:Vanguard:IRA 10.673 VFIFXG {127.64 USD}'
+		const [result, newLine] = calculate(line);
+		assert(result.equals(new Decimal('1362.30172')));
+		assert.deepStrictEqual(newLine, `${line} ; = 1362.30172`);
 	});
 });
